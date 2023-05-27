43b7e54fc7e19e103b0808ba60052bb0378c7a36
Grant L <grant.jy.li@gmail.com>
2023-05-26 20:16:06 -0400

    Decimal format done

--- Assignment1/OACP.c
+++ Assignment1/OACP.c
@@ -1,24 +1,30 @@
 #include<stdio.h>
+#include<math.h>
 
 //output function for output format
 void output(char (*ACD)[3]){
 	//integer to track decimal for output
 	int d=0;
 
+	//integer to track number of elements in the array
+	int s =(sizeof((*ACD)) / sizeof((*ACD)[0]));
+
 	printf("Your permissions are:\n");
 	printf("character format: %s\n", (*ACD));
 	printf("binary format: ");
 
-	for(int i=0; i< (sizeof((*ACD)) / sizeof((*ACD)[0])); i++){
-		//printf("%c", (*ACD)[i]);
+	//looping through the character array to print binary if each permission is yes or not
+	for(int i=0; i< s; i++){
 		
 		if ((*ACD)[i] == 'Y' || (*ACD)[i] == 'y'){
 			printf("1");
+			d += pow(2, s-i-1);
 		}else {
 			printf("0");
 		}
 	
 	}
+	printf("\nDecimal format: %d", d);
 }
 
 int main(){
