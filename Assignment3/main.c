6a53d1e487bd2824ca7bc4ab3ca9de7c2fa39ae6
BigTrash <grant.jy.li@gmail.com>
2023-07-12 00:57:06 -0400

    more added to main, started work on testing
    working on saving files while questions about querying multiple pokemon are being answered.

--- Assignment3/main.c
+++ Assignment3/main.c
@@ -11,47 +11,61 @@ typedef struct{
     int hp;
     int attack;
     int defence;
+    int specialAttack;
     int specialDefence;
     int speed;
     char generation;
     char legendary;
 } Pokemon;
 
-void getPokemon(){
+void getPokemon(Pokemon *pokeDeck){
+    char *input;
     printf("Enter the name of your chosen pokemon: ");
+    scanf("%m[^\n]", &input);
+
+
+    free(input);
 }
 
-void savePokemon(){
-
+void savePokemon(Pokemon *pokeDeck){
+    printf("chesses\n");
 
 }
 
-void menu(FILE *fp){
-    printf("\nEnter the number of the option you want:\n");
-    printf("1. Type search\n");
-    printf("2. Save results\n");
-    printf("3. Exit the program\n");
+int menu(FILE *fp, Pokemon *pokeDeck){
+    int deckSize = 0;
 
-    int choice;
-    scanf("%d", &choice);
+    do{
+        printf("\nMenu options:\n");
+        printf("1. Type search\n");
+        printf("2. Save results\n");
+        printf("3. Exit the program\n");
+        printf("Enter your option: ");
 
-    switch (choice)
-    {
-    case 1:
-        getPokemon();
-        break;
-    case 2:
-        savePokemon();
-        break;
-    default:
-        return;
-    }
+        int choice;
+        scanf("%d", &choice);
+
+        switch (choice){
+            case 1:
+                deckSize++;
+                realloc(pokeDeck, deckSize * sizeof(Pokemon));
+                getPokemon(pokeDeck);
+                break;
+            case 2:
+                savePokemon(pokeDeck);
+                break;
+            default:
+                return 1;
+        }
+    }while(1);
+
+    return 0;
 }
 
 int main(){
     char *input;
 
-    printf("please include file path from this directory and file prefix. i.e: \"./test.txt\"\n");
+    printf("please include file prefix. i.e: \"test.txt\"\n");
     printf("Enter destination file name: ");
     scanf("%m[^\n]", &input);
 
@@ -69,11 +83,15 @@ int main(){
         fp = fopen(input, "r");
     }
 
-    menu(fp);
+    Pokemon * pokeDeck = (Pokemon*)malloc(0);
+
+    menu(fp, pokeDeck);
+
     printf("\nEnding Program.");
     
     fclose(fp);
     free(input);
+    free(pokeDeck);
 
     return 0;
 }
--- Assignment3/test.c
+++ Assignment3/test.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    int number;
+    char *name;
+    char *type1;
+    char *type2;
+    int total;
+    int hp;
+    int attack;
+    int defence;
+    int specialAttack;
+    int specialDefence;
+    int speed;
+    char generation;
+    char legendary;
+} Pokemon;
+
+void save(Pokemon *pokeDeck){
+    printf("%d", pokeDeck[0].number);
+}
 
 int main(){
+    Pokemon *pokeDeck = malloc(sizeof(Pokemon));
+
+    Pokemon bulbasaur;
+    bulbasaur.number = 1;
+    bulbasaur.name = "bulbasaur";
+    bulbasaur.type1 = "Grass";
+    bulbasaur.type2 = "Poison";
+    bulbasaur.hp = 318;
+    bulbasaur.attack = 45;
+    bulbasaur.defence = 49;
+    bulbasaur.specialAttack = 49;
+    bulbasaur.specialDefence = 65;
+    bulbasaur.speed = 65;
+    bulbasaur.generation = 1;
+    bulbasaur.legendary = 0;
+
+    pokeDeck[0] = bulbasaur;
+
+    save(pokeDeck);
+    free(pokeDeck);
 }
